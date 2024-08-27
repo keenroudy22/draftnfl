b12665645a4956a34675cd87b3d17815ce65341c
keenroudy22 <kroudebush96@gmail.com>
2024-08-27 12:30:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -80,5 +80,10 @@ function moveToNextPlayer() {
 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
-    while (currentIndex !== 0)
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;  // Returning the shuffled array
 }
